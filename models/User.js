d22b2afe5e5555e94ebb4948a0e014be4a22bcf5
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-08-15 20:31:57 +0900

    style: fix styles

--- models/User.js
+++ models/User.js
@@ -1,5 +1,11 @@
 import { model, Schema } from 'mongoose';
 
+const scrypt = require('scryptsy');
+
+const salt = 'dddd';
+
+const hashPassword = (password) => scrypt(password, salt, 8192, 5, 1, 25).toString('hex');
+
 const UserSchema = new Schema({
   username: String,
   password: String,
@@ -8,5 +14,21 @@ const UserSchema = new Schema({
   timestamp: { type: Date, default: Date.now },
   profile: { type: String, default: 'https://inudevs.github.io/assets/images/profiles/default.jpg' },
 });
+
+// statics
+
+UserSchema.statics.create = function (username, password) {
+  const user = new this({
+    username,
+    password: hashPassword(password),
+  });
+  return user.save();
+};
+
+// methods
+
+UserSchema.methods.verify = function (password) {
+  return this.password === hashPassword(password);
+};
 
 export default model('User', UserSchema);
