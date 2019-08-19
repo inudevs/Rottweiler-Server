d1ee5e6bcf176eb102d42778147cfc89a64e1050
jaeseong-CS <shimjs8@naver.com>
2019-08-19 19:37:39 +0900

    refactor: remove comment

--- routes/api/board/index.js
+++ routes/api/board/index.js
@@ -1,11 +1,9 @@
 import { Router } from 'express';
 
 import create from './create';
-// import remove from './delete';
 
 const router = Router();
 
 router.use('/create', create);
-// router.use('/remove', remove);
 
 export default router;
